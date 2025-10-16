91c3654d1bd30688aec4f0250f1cc1d592c97338
Stefan00000-ux <gillstephen94@gmail.com>
2025-10-16 10:58:54 +0530

    Added image copy script

---
+++ copy_images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp ~/Downloads/img1.png ~/Stephen/sys-manager-demo/
+cp ~/Downloads/img2.png ~/Stephen/sys-manager-demo//
+cp ~/Downloads/img4.png ~/Stephen/sys-manager-demo/
+cp ~/Downloads/img5.png ~/Stephen/sys-manager-demo/
+cp ~/Downloads/img6.png ~/Stephen/sys-manager-demo/
